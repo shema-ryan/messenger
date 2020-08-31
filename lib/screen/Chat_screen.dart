08da012e3f171f57b1539585d4ddf0bc6ecb18ce
shema_ryan <kamanzishema@gmail.com>
2020-08-31 21:47:06 +0300

    Chat App

--- lib/screen/Chat_screen.dart
+++ lib/screen/Chat_screen.dart
@@ -1,34 +1,46 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        centerTitle: true,
-        title: Text('Messenger'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Firebase.initializeApp();
-          FirebaseFirestore.instance
+        appBar: AppBar(
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text('Messenger'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('/Chats/jmTvuPJxvAdOZkrSosJV/Messages')
+                .add({'Text': 'another message'});
+//            await Firebase.initializeApp();
+//              .listen((data) {
+//            print(data.docs[0].data()['Text']);
+//          });
+          },
+          child: Icon(Icons.add),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
               .collection('/Chats/jmTvuPJxvAdOZkrSosJV/Messages')
-              .snapshots()
-              .listen((data) {
-            print(data.docs[0].data()['Text']);
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-      body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) => Container(
-                padding: const EdgeInsets.all(10.0),
-                child: Text('app texting'),
-              )),
-    );
+              .snapshots(),
+          builder: (BuildContext context, snap) {
+            if (snap.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final docs = snap.data.docs;
+            return ListView.builder(
+                itemCount: docs.length,
+                itemBuilder: (BuildContext context, int index) => Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(docs[index].data()['Text']),
+                    ));
+          },
+        ));
   }
 }
