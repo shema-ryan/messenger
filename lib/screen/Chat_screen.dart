58719c0a2689e256890663f7586ba3beeeaff344
shema_ryan <kamanzishema@gmail.com>
2020-08-31 18:39:14 +0300

    Chat App

--- lib/screen/Chat_screen.dart
+++ lib/screen/Chat_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         centerTitle: true,
-        title: Text('S - Messenger'),
+        title: Text('Messenger'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('/Chats/jmTvuPJxvAdOZkrSosJV/Messages')
+              .snapshots()
+              .listen((data) {
+            print(data);
+          });
+        },
+        child: Icon(Icons.add),
       ),
       body: ListView.builder(
           itemCount: 5,
