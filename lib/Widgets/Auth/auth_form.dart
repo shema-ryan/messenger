cd9fcacab280735f8ae38517f3a95b5b8b7b938c
shema_ryan <kamanzishema@gmail.com>
2020-09-09 18:50:25 +0300

    Chat App

--- lib/Widgets/Auth/auth_form.dart
+++ lib/Widgets/Auth/auth_form.dart
@@ -3,18 +3,39 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 class AuthForm extends StatefulWidget {
+  final void Function(String email, String password, String username,
+      bool login, BuildContext context) submit;
+  AuthForm(this.submit);
   @override
   _AuthFormState createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
+  // Helper variables
   final scaffoldKey = GlobalKey<FormState>();
-  bool ob = true;
+  bool hidePassword = true;
+  bool login = true;
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  // Helper functions
+  void _trySubmit() {
+    final bool valBool = scaffoldKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+    if (valBool) {
+      scaffoldKey.currentState.save();
+      widget.submit(_userEmail.trim(), _userPassword.trim(), _userName.trim(),
+          login, context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
-        margin: EdgeInsets.all(20),
+        color: Colors.white54,
+        margin: EdgeInsets.all(10),
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.all(16),
@@ -24,25 +45,62 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('userMail'),
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Email not valid';
+                      }
+                      return null;
+                    },
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: Theme.of(context).primaryColor,
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       icon: Icon(Icons.mail),
                       labelText: 'email address',
                     ),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'user name',
-                      icon: Icon(Icons.person),
+                  if (!login)
+                    TextFormField(
+                      key: ValueKey('userName'),
+                      onSaved: (value) {
+                        _userName = value;
+                      },
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'name must be of 4 characters least';
+                        }
+                        return null;
+                      },
+                      style: TextStyle(color: Colors.white),
+                      cursorColor: Theme.of(context).primaryColor,
+                      decoration: InputDecoration(
+                        labelText: 'user name',
+                        icon: Icon(Icons.person),
+                      ),
                     ),
-                  ),
                   TextFormField(
-                    obscureText: ob,
+                    key: ValueKey('password'),
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
+                    validator: (value) {
+                      if (value.length < 7) {
+                        return 'password must be of 7 characters least ';
+                      }
+                      return null;
+                    },
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: Theme.of(context).primaryColor,
+                    obscureText: hidePassword,
                     decoration: InputDecoration(
                       suffixIcon: GestureDetector(
                         onTap: () {
                           setState(() {
-                            ob = !ob;
+                            hidePassword = !hidePassword;
                           });
                         },
                         child: Icon(Icons.remove_red_eye),
@@ -52,22 +110,63 @@ class _AuthFormState extends State<AuthForm> {
                     ),
                   ),
                   SizedBox(
-                    height: 20,
+                    height: 40,
                   ),
-                  RaisedButton(
-                    textColor: Colors.white60,
-                    color: Theme.of(context).primaryColor,
-                    mouseCursor: MouseCursor.uncontrolled,
-                    child: Text('Login'),
-                    onPressed: () {},
-                  ),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.person_add,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    label: Text('create an account'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            login = true;
+                          });
+                          if (login = true) {
+                            _trySubmit();
+                          }
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 140,
+                          child: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            login = false;
+                          });
+                          if (login == false) {
+                            _trySubmit();
+                          }
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 140,
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor),
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              style: BorderStyle.solid,
+                              width: 3,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
--- lib/screen/Auth_screen.dart
+++ lib/screen/Auth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../Widgets/Auth/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -7,24 +9,56 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  void _submit(String email, String password, String userName, bool log,
+      BuildContext ctx) async {
+    final _auth = FirebaseAuth.instance;
+    UserCredential _userCredential;
+    try {
+      if (log == true) {
+        _userCredential = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        print(_userCredential);
+      } else {
+        _userCredential = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException catch (error) {
+      var message = 'an Error occurred please check your credentials';
+      if (error.message != null) {
+        message = error.message;
+        Scaffold.of(ctx).showSnackBar(SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ));
+      }
+    } catch (error) {
+      Scaffold.of(ctx).showSnackBar(SnackBar(
+        content: Text(error.message),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        alignment: Alignment.center,
         children: [
           Container(
+            height: double.infinity,
+            width: double.infinity,
             child: DecoratedBox(
               decoration: BoxDecoration(
-                color: Colors.green,
+                color: Theme.of(context).primaryColor.withOpacity(0.5),
               ),
             ),
             decoration: BoxDecoration(
                 image: DecorationImage(
+                    fit: BoxFit.fitHeight,
                     image: NetworkImage(
-                        'https://cdn.shopify.com/s/files/1/0027/8645/9757/products/Cloud2021_1800x1800.jpg'))),
+                      'https://cdn.shopify.com/s/files/1/0027/8645/9757/products/Cloud2021_1800x1800.jpg',
+                    ))),
           ),
-          AuthForm(),
+          AuthForm(_submit),
         ],
       ),
     );
